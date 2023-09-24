96332bfb6ce3632b2a16325a3cb4213606119b1a
eunji1 <ssak1088@gmail.com>
2023-09-24 11:52:09 +0900

    first commit

---
+++ Section3/app.js
@@ -0,0 +1,17 @@
+// 서버 생성
+// http 모듈 불러오기
+
+const http = require("http");
+
+// createsever callback 함수
+// 서버에 요청이 들어볼 때마다 nodeJS가 호출하게 됨
+// EDA 표현 기법
+http.createServer(function (req, res) {});
+http.createServer((req, res) => {});
+
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+// 포트 지정
+server.listen(3000);
