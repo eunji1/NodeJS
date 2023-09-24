939140ad4242096c294b17197e41394b64599978
성은지 <96868722+eunji1@users.noreply.github.com>
2023-09-25 00:21:42 +0900

    section3 요청 본문 분석 스트림과 버퍼

--- Section3/app.js
+++ Section3/app.js
@@ -26,7 +26,18 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("messate.txt", "DUMMY");
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      // message는 key-value 값으로 저장된다.
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
     res.statusCode = 302;
     res.setHeader("Location", "/");
     return res.end();
