08c95eeec227a39562888b98d8f3383506efcf14
eunji1 <ssak1088@gmail.com>
2023-10-26 15:40:42 +0900

    section 11 마무리

--- Section11/README.md
+++ Section11/README.md
@@ -1,21 +1,23 @@
 # Section 10: SQL 소개
 
 ### 135. 모듈 소개
+
 - SQL NoSQL 알아보기
 
 ### 136. 데이터 베이스 선택하기
+
 - 우리의 목표: 데이터를 저장하고 쉽게 접근가능하게 하기
   - 데이터 베이스 사용
 
-||||
-|---|---|---|
-||SQL|NoSQL|
-|SQL과 NoSQL의 차이|e.g. MySQL|e.g. MongoDB|
-||Data uses Schemas|Schema-less|
-||Relations|No relations|
-||Data is disreibuted across multiple tables|중첩된 문서작성 -> nesty|
-||Horizontal scaling이 어렵다.<br> Vertical scaling is possible|Both Horizontal Vertical possible|
-||매초 다수 혹은 수천건의 쿼리를 읽고 쓰면 문제가 발생할 수 있다.|읽고 쓰기에 좋은 성능|
+|                    |                                                                 |                                   |
+| ------------------ | --------------------------------------------------------------- | --------------------------------- |
+|                    | SQL                                                             | NoSQL                             |
+| SQL과 NoSQL의 차이 | e.g. MySQL                                                      | e.g. MongoDB                      |
+|                    | Data uses Schemas                                               | Schema-less                       |
+|                    | Relations                                                       | No relations                      |
+|                    | Data is disreibuted across multiple tables                      | 중첩된 문서작성 -> nesty          |
+|                    | Horizontal scaling이 어렵다.<br> Vertical scaling is possible   | Both Horizontal Vertical possible |
+|                    | 매초 다수 혹은 수천건의 쿼리를 읽고 쓰면 문제가 발생할 수 있다. | 읽고 쓰기에 좋은 성능             |
 
 ### 140. 앱을 SQL 데이터베이스에 연결하기
 
@@ -24,6 +26,7 @@ npm install --save mysql2
 ```
 
 `/util/datebase`
+
 ```js
 const mysql = require("mysql2");
 
@@ -35,27 +38,27 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
-
 ```
+
 - pool에 유저 정보를 저장한다.
 - - createPool:
 
 execute method
+
 ```js
-  // products query 가져오기
-  db.execute("SELECT * FROM products");
+// products query 가져오기
+db.execute("SELECT * FROM products");
 
-  // (WHERE) product.id가 id 와 같은 products query 가져오기
-  db.execute("SELECT * PROM products WHERE products.id = ?, [id]");
+// (WHERE) product.id가 id 와 같은 products query 가져오기
+db.execute("SELECT * PROM products WHERE products.id = ?, [id]");
 
-  // 데이터 삽입 method
-  db.execute(
-      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    ); 
-
-
+// 데이터 삽입 method
+db.execute(
+  "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
+  [this.title, this.price, this.imageUrl, this.description]
+);
 ```
+
 ### 참고
 
 - [MySQL/SQL에 대해 전반적으로 알아보기](https://www.w3schools.com/sql/)
@@ -76,9 +79,10 @@ execute method
 npm install --save sequelize
 ```
 
-- [x] : *MySQL 데이터베이스에 저장된 product 테이블을 삭제해야한다.*
+- [x] : _MySQL 데이터베이스에 저장된 product 테이블을 삭제해야한다._
 
 ### 152. 모델 정의
+
 ```js
 const Sequelize = require("sequelize");
 
@@ -98,6 +102,7 @@ modules.exports = Product;
 ```
 
 ### 155. 필독 Sequelize 5의 findById()
+
 유의사항.
 
 - Sequelize v5 이용 시, 강의에서 사용할 `findById()` 대신 `findByPk()`를 사용합니다.
@@ -112,3 +117,25 @@ modules.exports = Product;
   - 물건(Order)
     - product 하나의 물건을 가짐
     - user 여러 물건을 가짐
+
+### 172. 장바구니 상품을 주문 상품으로 저장하기
+
+```js
+// controllers/shop
+
+req.user.createOrder();
+```
+
+- order model을 불러올 필요없이 `user.createOder` 면 자동으로 불러와진다.
+
+### 174. 마무리
+
+SQL
+
+- 엄격한 데이터 스키마와 데이터베이스 관계
+  Sequelize
+- SQL 쿼리 메뉴얼 대신에 node.js 코드에 맞추어 js 객체 형식으로 사용
+
+### 175. 참고자료
+
+- [Sequelize 공식 참고자료](http://docs.sequelizejs.com/)
--- Section11/controllers/shop.js
+++ Section11/controllers/shop.js
@@ -1,16 +1,15 @@
-const Product = require("../models/product");
-const Cart = require("../models/cart");
+const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
-    .then((products) => {
-      res.render("shop/product-list", {
+    .then(products => {
+      res.render('shop/product-list', {
         prods: products,
-        pageTitle: "All Products",
-        path: "/products",
+        pageTitle: 'All Products',
+        path: '/products'
       });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 };
@@ -18,36 +17,35 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   // Product.findAll({ where: { id: prodId } })
-  //   .then((products) => {
-  //     res.render("shop/product-detail", {
+  //   .then(products => {
+  //     res.render('shop/product-detail', {
   //       product: products[0],
   //       pageTitle: products[0].title,
-  //       path: "/products",
+  //       path: '/products'
   //     });
   //   })
-  //   .catch((err) => console.log(err));
-  // findById 대신 findByPk
-  Product.findByPk(prodId)
-    .then((product) => {
-      res.render("shop/product-detail", {
+  //   .catch(err => console.log(err));
+  Product.findById(prodId)
+    .then(product => {
+      res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
-        path: "/products",
+        path: '/products'
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
   Product.findAll()
-    .then((products) => {
-      res.render("shop/index", {
+    .then(products => {
+      res.render('shop/index', {
         prods: products,
-        pageTitle: "Shop",
-        path: "/",
+        pageTitle: 'Shop',
+        path: '/'
       });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
     });
 };
@@ -55,64 +53,111 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then((cart) => {
-      return cart.getProducts().then((products) => {
-        res.render("shop/cart", {
-          path: "/cart",
-          pageTitle: "Your Cart",
-          products: products,
-        });
-      });
+    .then(cart => {
+      return cart
+        .getProducts()
+        .then(products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+        })
+        .catch(err => console.log(err));
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user
     .getCart()
-    .then((cart) => {
+    .then(cart => {
       fetchedCart = cart;
       return cart.getProducts({ where: { id: prodId } });
     })
-    .then((products) => {
+    .then(products => {
       let product;
-      if (products.length > 0) product = products[0];
-      let newQuantity = 1;
-      if (product) {
-        // ...
+      if (products.length > 0) {
+        product = products[0];
       }
-      return Product.findByPk(prodId)
-        .then((product) => {
-          return fetchedCart.addProduct(product, {
-            through: { quantity: newQuantity },
-          });
-        })
-        .catch((err) => console.log(err));
+
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findById(prodId);
     })
-    .then(() => res.redirect("/cart"))
-    .catch((err) => console.log(err));
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req.user
+        .createOrder()
+        .then(order => {
+          return order.addProducts(
+            products.map(product => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", {
-    path: "/orders",
-    pageTitle: "Your Orders",
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", {
-    path: "/checkout",
-    pageTitle: "Checkout",
-  });
+  req.user
+    .getOrders({include: ['products']})
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
 };
